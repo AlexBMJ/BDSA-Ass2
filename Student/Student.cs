44a0e97aacd7919e4a9d7b1b58b1f0bdd9e58d5c
AlexBMJ <alexander.magno.jacobsen@gmail.com>
2021-09-17 14:14:30 +0200

    Setup project files and implement properties

---
+++ Student/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Student
+{
+    public class Student
+    {
+        public int Id { get; init; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public string Status { get; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime GraduationDate { get; set; }
+
+        public string toString() {
+            return $"Id: {Id.ToString()}, Name: {GivenName}, Surname: {Surname}, Status: {Status}, Start Date: {StartDate.ToString()}, End Date: {EndDate.ToString()}, Graduation Date: {GraduationDate.ToString()}"
+        }
+    }
+
+    enum Status {
+        New,
+        Active,
+        Dropout,
+        Graduated
+    }
+}
