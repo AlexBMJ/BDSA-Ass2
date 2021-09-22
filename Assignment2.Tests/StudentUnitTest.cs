efaca10a8f1daceb81b11c5755719221b55c17ca
alexander.rode2 <maler2@live.dk>
2021-09-22 16:32:30 +0200

    +Fix no "AM" in expected

--- Assignment2.Tests/StudentUnitTest.cs
+++ Assignment2.Tests/StudentUnitTest.cs
@@ -59,7 +59,7 @@ namespace Assignment2.Tests
             };
 
             string output = Istudent.ToString();
-            string expected = "ImmutableStudent { Id = 1, GivenName = John, Surname = Doe, Status = Active, StartDate = 08/24/2020 00:00:00 AM, EndDate = 07/23/2023 00:00:00 AM, GraduationDate = 07/15/2023 00:00:00 AM }";
+            string expected = "ImmutableStudent { Id = 1, GivenName = John, Surname = Doe, Status = Active, StartDate = 08/24/2020 00:00:00, EndDate = 07/23/2023 00:00:00, GraduationDate = 07/15/2023 00:00:00 }";
 
             Assert.Equal(expected, output);
         }
