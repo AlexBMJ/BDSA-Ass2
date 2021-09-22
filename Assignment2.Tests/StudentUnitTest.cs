66a0e68b6e5c321db707afffbc7f525d32791253
alexander.rode2 <maler2@live.dk>
2021-09-22 16:09:53 +0200

    +GraduationDate in record ToString test

--- Assignment2.Tests/StudentUnitTest.cs
+++ Assignment2.Tests/StudentUnitTest.cs
@@ -47,18 +47,19 @@ namespace Assignment2.Tests
         }
 
         [Fact]
-        public void ImmutableStudent_ToString_Returns() {
+        public void ImmutableStudent_ToString_Returns_Properties() {
             var Istudent = new ImmutableStudent() 
             {
                 Id = 1,
                 GivenName = "John",
                 Surname = "Doe",
                 StartDate = new DateTime(2020, 8, 24),
-                EndDate = new DateTime(2023, 7, 23)
+                EndDate = new DateTime(2023, 7, 23),
+                GraduationDate = new DateTime(2023, 7, 15)
             };
 
             string output = Istudent.ToString();
-            string expected = "ImmutableStudent { Id = 1, GivenName = John, Surname = Doe, Status = Active, StartDate = 8/24/2020 12:00:00 AM, EndDate = 7/23/2023 12:00:00 AM, GraduationDate = 1/1/0001 12:00:00 AM }";
+            string expected = "ImmutableStudent { Id = 1, GivenName = John, Surname = Doe, Status = Active, StartDate = 8/24/2020 12:00:00 AM, EndDate = 7/23/2023 12:00:00 AM, GraduationDate = 7/15/2023 12:00:00 AM }";
 
             Assert.Equal(expected, output);
         }
